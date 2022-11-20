4bcbc473824c3de179d255dc796dd89ab9307a1c
isaac-cabrices <isaac.cabrices@gmail.com>
2022-11-20 07:44:00 +0000

    test adjustments so it all uses pytest

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -4,7 +4,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import *
 
 from App.main import create_app
-from App.database import create_db
+from App.database import create_db, db
 from App.models import User, Author, Publication
 from App.controllers import (
     create_user,
@@ -29,36 +29,47 @@ LOGGER = logging.getLogger(__name__)
 '''
    Unit Tests
 '''
-class UserUnitTests(unittest.TestCase):
+## Fixtures ##
+@pytest.fixture
+def setup_User_1():
+    testUsername = "bob"
+    testPassword = "bobpass"
+    testID = 1
+    user1 = User(testUsername, testPassword, testID)
+    return user1
 
-    def test_new_user(self):
-        user = User("bob", "bobpass", 1)
+@pytest.fixture
+def setup_User_2():
+    testUsername = "rick"
+    testPassword = "bobpass"
+    testID = 2
+    user2 = User(testUsername, testPassword, testID)
+    return user2
+## end fixtures ##
+
+
+class UserUnitTests():
+
+    def test_new_user(setup_User):
         assert user.username == "bob"
 
-    def test_user_toJSON(self):
-        user = User("bob", "bobpass", 1)
+    def test_user_toJSON(setup_User):
         user_json = user.toJSON()
-        self.assertDictEqual(user_json, {"id":None, "username":"bob", "authorId":1})
+        assert user_json == {"id":None, "username":"bob", "authorId":1}
     
     def test_hashed_password(self):
-        password = "mypass"
         hashed = generate_password_hash(password, method='sha256')
-        user = User("bob", password, 1)
         assert user.password != password
 
     def test_check_password(self):
-        password = "mypass"
-        user = User("bob", password, 1)
         assert user.check_password(password)
 
 class AuthorUnitTests(unittest.TestCase):
 
-    def test_new_author(self):
-        author = Author("Bob Moog", "05/08/2001", "BSc. Computer Science")
+    def test_new_author(setupAuthor):
         assert author.name == "Bob Moog" and author.dob == datetime.strptime("05/08/2001", "%d/%m/%Y") and author.qualifications == "BSc. Computer Science"
 
     def test_author_toJSON(self):
-        author = Author("Bob Moog", "05/08/2001", "BSc. Computer Science")
         author_json = author.toJSON()
         self.assertDictEqual(author_json, {
             "id": None,
@@ -104,41 +115,55 @@ class PublicationUnitTests(unittest.TestCase):
 def empty_db():
     app.config.update({'TESTING': True, 'SQLALCHEMY_DATABASE_URI': 'sqlite:///test.db'})
     create_db(app)
+    create_all(app)
     yield app.test_client()
     os.unlink(os.getcwd()+'/App/test.db')
+
+@pytest.fixture(scope="module")
+def setup_Authors():
+    #author 1
+    testName1 = "Bob Moog"
+    testDoB1 = "05/08/2001"
+    testQualifications1 = "BSc. Computer Science"
+    author1 = Author(testName1, testDoB1, testQualifications1)
+
+    #author 2
+    testName2 = "Bob Moog"
+    testDoB2 = "05/08/2001"
+    testQualifications2 = "BSc. Computer Science"
+    author2 = Author(testName2, testDoB2, testQualifications2)
+    
+
 
 
 # def test_authenticate():
 #     user = create_user("bob", "bobpass")
 #     assert authenticate("bob", "bobpass") != None
 
-class UsersIntegrationTests(unittest.TestCase):
-    def test_authenticate(self):
-        user = create_user("bob", "bobpass", 1)
+class UsersIntegrationTests():
+    def test_authenticate(setup_User_1):
         assert authenticate("bob", "bobpass") != None
 
-    def test_create_user(self):
+    def test_create_user(setup_User_2):
         # user = create_user("bob", "bobpass")
-        user = create_user("rick", "bobpass", 2)
         assert user.username == "rick"
 
-    def test_get_all_users_json(self):
+    def test_get_all_users_json(setup_User_1, setup_User_2):
         users_json = get_all_users_json()
-        self.assertListEqual([{"id":1, "username":"bob", "authorId":1}, {"id":2, "username":"rick", "authorId":2}], users_json)
+        assert [{"id":1, "username":"bob", "authorId":1}, {"id":2, "username":"rick", "authorId":2}] == users_json
 
     # Tests data changes in the database
-    def test_update_user(self):
+    def test_update_user(setup_User_1):
         update_user(1, "ronnie")
         user = get_user(1)
         assert user.username == "ronnie"
 
 
-class AuthorIntegrationTests(unittest.TestCase):
-    def test_create_author(self):
-        author = create_author("Bob Moog", "05/08/2001", "BSc. Computer Science")
+class AuthorIntegrationTests():
+    def test_create_author(setup_Author):
         assert author.name == "Bob Moog"
 
-    def test_get_all_authors_json(self):
+    def test_get_all_authors_json(setup_Author):
         author_json=get_all_authors_json()
         self.assertListEqual([{
             'id': 1,
@@ -148,8 +173,8 @@ class AuthorIntegrationTests(unittest.TestCase):
             }
             ], author_json)
 
-class PublicationIntegrationTests(unittest.TestCase):
-    def test_create_publication(self):
+class PublicationIntegrationTests():
+    def test_create_publication(setup_Author):
         author = get_author(1)
         if not author:
             author = create_author("Bob Moog", "05/08/2001", "BSc. Computer Science")
