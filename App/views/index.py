4b66e65dc6fbba14be5bf663c9d9ef23e76f5a03
crowbar72 <tristanleid72@gmail.com>
2022-11-26 02:15:47 +0000

    Added routes to pages

--- App/views/index.py
+++ App/views/index.py
@@ -5,3 +5,23 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 @index_views.route('/', methods=['GET'])
 def index_page():
     return render_template('index.html')
+
+@index_views.route('/signuppage', methods=['GET'])
+def signup_page():
+    return render_template('signup.html')
+
+@index_views.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+@index_views.route('/newpub', methods=['GET'])
+def new_publication_page():
+    return render_template('newpub.html')
+
+@index_views.route('/results', methods=['GET'])
+def search_results_page():
+    return render_template('results.html')
+
+@index_views.route('/pubtree', methods=['GET'])
+def search_results_page():
+    return render_template('pubtree.html')
