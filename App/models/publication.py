28db4d5a99773407b54bf64aa7b67ca86b85e3ab
Zubayr-Imkaraaz <z.s.imkaraaz@gmail.com>
2022-11-28 05:05:11 +0000

    yes

--- App/models/publication.py
+++ App/models/publication.py
@@ -8,6 +8,8 @@ class Publication(db.Model):
     title = db.Column(db.String(120), nullable=False, unique=True)
     authorId = db.Column(db.Integer, db.ForeignKey("author.id"), nullable=False)
     coauthors = db.relationship("Author", secondary=CoAuthorPublication)
+    abstract = db.Column(db.String(200), nullable=False, unique=True)
+    dateOfPublication = db.Column(db.Date, nullable=False, unique=True)
 
     def __init__(self, title, authorId, coauthors):
         self.title = title
@@ -20,5 +22,7 @@ class Publication(db.Model):
             "id": self.id,
             "title": self.title, 
             "author": self.authorId,
-            "coauthors": [coauthor.toJSON() for coauthor in self.coauthors]
+            "coauthors": [coauthor.toJSON() for coauthor in self.coauthors],
+            "abstract": self.abstract,
+            "dateOfPublication": self.dateOfPublication
         }
