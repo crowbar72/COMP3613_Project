87ca4447917328ac2fd49ca3678756b3665a931b
isaac-cabrices <isaac.cabrices@gmail.com>
2022-12-07 01:43:13 -0400

    Update create_user
    Updated to match the changes in the model.

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -2,16 +2,12 @@ from App.models import User, Author
 from App.database import db
 
 
-def create_user(username, password, authorId):
-    newuser = User(username=username, password=password, authorId=authorId)
+def create_user(name, username, password, authorId):
+    newuser = User(name=name, username=username, password=password, authorId=authorId)
     author = Author.query.filter_by(name=name).first()
-    author.id = authorId
-    
-    
-    
+    #author.id = authorId
     db.session.add(newuser)
     db.session.commit()
-
     return newuser
 
 def get_user_by_username(username):
